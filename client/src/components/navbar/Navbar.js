1d1c35e1fa6b6b389aaad98bf0cc0c97e1c4efff
Aldwin Lubrica <aldwin.lub@gmail.com>
2021-08-21 08:26:27 -0700

    Added HTML and css to navbar, login, and register. The chatHompage still needs css

---
+++ client/src/components/navbar/Navbar.js
@@ -0,0 +1,35 @@
+import "./navbar.css";
+import { Search, AccountBox, People, Chat } from "@material-ui/icons";
+
+export default function Navbar() {
+  return (
+    <div className="navbarContainer">
+      <div className="navbarLeft">
+        <span className="logo">Chat App</span>
+      </div>
+      <div className="navbarCenter">
+        <div className="searchbar">
+          <Search />
+          <input placeholder="Search for Chatters" className="searchInput" />
+        </div>
+      </div>
+      <div className="navbarRight">
+        <div className="navbarIcons">
+          <div className="navbarIconItem">
+            <Chat />
+            <span className="navbarIconBadge">Chat</span>
+          </div>
+          <div className="navbarIconItem">
+            <People />
+            <span className="navbarIconBadge">Friends</span>
+          </div>
+          <div className="navbarIconItem">
+            <AccountBox />
+            <span className="navbarIconBadge">Profile</span>
+          </div>
+        </div>
+        <img src="" alt="" className="navbarImg" />
+      </div>
+    </div>
+  );
+}
