3c36ea7e9266df0182f2a32316a1561e9ac62bff
poly singh <poly.singh11@gmail.com>
2021-08-21 09:11:30 -0700

    merging main

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+const socket =require('./socket');
 
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
