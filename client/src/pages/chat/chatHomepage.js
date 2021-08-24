96fd1b0421101fa1f6f258ebaaaa79f1b05323f0
James <James.Hacklander@gmail.com>
2021-08-23 17:01:34 -0700

    Trying to fix user state

--- client/src/pages/chat/chatHomepage.js
+++ client/src/pages/chat/chatHomepage.js
@@ -2,6 +2,13 @@ import Navbar from "../../components/navbar/Navbar";
 import React from "react";
 // import { useQuery } from '@apollo/client';
 
-export default function chatHomepage() {
-  return <Navbar />;
+export default function chatHomepage( {user}) {
+  console.log(user)
+  return (
+  <div>
+    <Navbar />
+    <h1>Hello</h1>
+  </div>
+);
 }
+
