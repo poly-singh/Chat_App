d62a9614d42416d8dd9447393ee40e58ecfdb0ea
poly singh <poly.singh11@gmail.com>
2021-08-24 13:08:01 -0700

    adding the username

--- client/src/components/message/message.js
+++ client/src/components/message/message.js
@@ -1,12 +1,18 @@
 import "./message.css";
+import Auth from "../../utils/auth";
 
 const Message = ({ own, messageAuthor, messageText }) => {
   console.log(own, "===own===");
+  const { username } = Auth.getProfile().data;
   return (
     <div className={own ? "message own" : "message"}>
       <div className="messageTop">
-        <img className="messageImg" src="" alt="" />
+        {/* <img className="messageImg" src="" alt="" /> */}
+        <div class="username">
+          <p className="username">{username}</p>
+        </div>
         <p className="messageText">{messageText}</p>
+        
       </div>
       <div className="messageBottom"></div>
     </div>
