cf6b95fd2b2710dff0069037ce544327a0a9aed8
James <James.Hacklander@gmail.com>
2021-08-23 21:46:22 -0700

    fixed formatting

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -2,37 +2,37 @@ import { gql } from '@apollo/client';
 
 export const QUERY_USER = gql`
     query user($username: String!) {
-            user(username: $username) {
-                _id
-                username
-                email
-                messages {
-                        _id
-                        messageText
-                        createdAt
-                }
+        user(username: $username) {
+            _id
+            username
+            email
+            messages {
+                    _id
+                    messageText
+                    createdAt
             }
+        }
     }
 `;
 
 export const QUERY_MESSAGES = gql`
     query getMessages {
-            messages {
-                _id
-                messageText
-                messageAuthor
-                createdAt
-            }
+        messages {
+            _id
+            messageText
+            messageAuthor
+            createdAt
+        }
     }
 `;
 
 export const QUERY_SINGLE_MESSAGE = gql`
     query getSingleMessage($messageId: ID!) {
         message(messageId: $messageId) {
-                _id
-                messageText
-                messageAuthor
-                createdAt
-            }
+            _id
+            messageText
+            messageAuthor
+            createdAt
+        }
     }
 `;
